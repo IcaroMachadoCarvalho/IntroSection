e55e9027e64d10e376ec5744bf513ac567455913
IcaroMachadoCarvalho <icarocarvalho028@gmail.com>
2024-03-14 16:09:10 -0300

    conteudo geral responsivo v2

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
+// Menu items
 const openMenu = document.getElementsByClassName("header__menu")[0];
 const sideNav = document.getElementsByClassName("header__options")[0];
 const closeMenu = document.getElementsByClassName("header__close")[0];
 const backGround = document.getElementsByClassName("background")[0];
-
+// Dropdowns
 const featuresHover = document.getElementById("features");
 const companyHover = document.getElementById("company");
 
+// Mouse over on features dropdown changes the icon
 featuresHover.addEventListener("mouseover", () => {
   document.getElementById("featuresIcon").src =
-    "./src/assets/icons/icon-arrow-up.svg";
+  "./src/assets/icons/icon-arrow-up.svg";
 });
 
 featuresHover.addEventListener("mouseout", () => {
   document.getElementById("featuresIcon").src =
-    "./src/assets/icons/icon-arrow-down.svg";
+  "./src/assets/icons/icon-arrow-down.svg";
 });
 
+// Mouse over on company dropdown changes the icon
 companyHover.addEventListener("mouseover", () => {
   document.getElementById("companyIcon").src =
     "./src/assets/icons/icon-arrow-up.svg";
@@ -26,9 +29,9 @@ companyHover.addEventListener("mouseout", () => {
     "./src/assets/icons/icon-arrow-down.svg";
 });
 
+// Check if the user is on mobile screen
 if (window.innerWidth <= 809) {
   openMenu.addEventListener("click", () => {
-    // console.log("Abre");
     sideNav.style.width = "50%";
     closeMenu.style.display = "block";
     closeMenu.style.visibility = "visible";
@@ -37,26 +40,22 @@ if (window.innerWidth <= 809) {
   });
 
   closeMenu.addEventListener("click", () => {
-    // console.log("Fecha");
     sideNav.style.width = "0px";
     closeMenu.style.visibility = "hidden";
     backGround.style.display = "none";
     backGround.style.visibility = "hidden";
   });
 }
-
-window.addEventListener("resize", verificar);
-function verificar() {
-  // console.log(window.innerWidth);
+// Check if the user is on desktop screen
+window.addEventListener("resize", verify);
+function verify() {
   if (window.innerWidth > 809) {
-    // sideNav.style.backGround = "visible";
     sideNav.style.display = "flex";
     sideNav.style.width = "90%";
     openMenu.style.display = "none";
     closeMenu.style.display = "none";
     backGround.style.visibility = "hidden";
   } else if (sideNav.style.width === "90%") {
-    // sideNav.style.visibility = "hidden";
     openMenu.style.display = "block";
     closeMenu.style.display = "none";
     sideNav.style.display = "flex";
